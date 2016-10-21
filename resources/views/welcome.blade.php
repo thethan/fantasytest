39572b08cf2a36499962d65c2654321c977ee1bd
ethan <ethan@selftotten.com>
2016-10-21 06:48:45 -0700

    basic home page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+@font-face {
+    font-family: 'burfordbold';
+    src: url('/fonts/burfordrusticbookbold-webfont.eot');
+    src: url('/fonts/burfordrusticbookbold-webfont.eot?#iefix') format('embedded-opentype'),
+         url('/fonts/burfordrusticbookbold-webfont.woff2') format('woff2'),
+         url('/fonts/burfordrusticbookbold-webfont.woff') format('woff'),
+         url('/fonts/burfordrusticbookbold-webfont.ttf') format('truetype'),
+         url('/fonts/burfordrusticbookbold-webfont.svg#burfordrusticbookbold') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+            html, body {
+                font-family: 'burfordbold', arial, sans-serif;
+                background-color: #008BCE;
+                color: #fff;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+
+            <div class="content">
+                <div class="title m-b-md">
+                    <?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     viewBox="0 0 600 600" enable-background="new 0 0 600 600" xml:space="preserve">
+<g id="Layer_9">
+</g>
+<g id="Face">
+    <path id="XMLID_24_" fill-rule="evenodd" clip-rule="evenodd" fill="#CF9E76" d="M399.1,366c0,87.2-56.3,158-125.8,158
+        s-125.8-70.7-125.8-158s-31.7-164.1,125.8-158C432.8,214.2,399.1,278.8,399.1,366z"/>
+    <g id="eyes">
+        <circle id="XMLID_25_" fill-rule="evenodd" clip-rule="evenodd" fill="#232323" cx="220" cy="312" r="12"/>
+        <circle id="XMLID_27_" fill-rule="evenodd" clip-rule="evenodd" fill="#232323" cx="309" cy="312" r="12"/>
+    </g>
+    <g id="Layer_5">
+        <path id="XMLID_36_" fill="none" stroke="#7D4921" stroke-miterlimit="10" d="M299,453.5c0,0-37.2,1.5-49.5,0
+            c-3.1-0.4-12.2-3.1-12.2-3.1"/>
+    </g>
+    <g id="Beard">
+        <path id="XMLID_31_" fill="#DDDAD9" d="M159,274c0,0,13,137,31,163s42.7,36,72.3,39s48.9-5,67.8-19s38.9-28,43.9-64
+            s8.2-125.2,8.2-125.2l25.8,2.7c0,0,0.1,127.5-9.9,155.5s-31.5,65.7-70.5,83.7c-39,18-87.5,25.3-119.5,7.3s-56.3-64-63.1-97
+            s-6.3-151.3-6-151.3c0.4,0,16.1-6.7,16.1-6.7L159,274z"/>
+        <path id="XMLID_30_" fill="#DDDAD9" d="M190,448c0,0,42.2-15,78.7-15s82.7,15,82.7,15s-17.8-44-83.7-41
+            C207.8,409.7,190,448,190,448z"/>
+        <g id="beard_dark">
+        </g>
+    </g>
+</g>
+<g id="pipe">
+    <polygon id="XMLID_1274_" fill="#8B4324" points="383.8,442.8 377.6,474.7 427.6,484.4 433.8,452.6    "/>
+
+        <ellipse id="XMLID_1273_" transform="matrix(0.9813 0.1923 -0.1923 0.9813 93.7303 -70.2722)" fill="#75351F" cx="408.8" cy="447.6" rx="25.5" ry="7.7"/>
+
+        <ellipse id="XMLID_1272_" transform="matrix(0.9813 0.1923 -0.1923 0.9813 99.736 -68.4779)" fill="#8B4324" cx="402.6" cy="479.5" rx="25.5" ry="8.6"/>
+
+        <ellipse id="XMLID_1271_" transform="matrix(0.9813 0.1923 -0.1923 0.9813 93.7303 -70.2722)" fill="#574109" cx="408.8" cy="447.6" rx="18.4" ry="5.5"/>
+
+        <rect id="XMLID_1270_" x="284.9" y="459.4" transform="matrix(0.9813 0.1923 -0.1923 0.9813 95.021 -55.2438)" fill="#75351F" width="94.3" height="4.8"/>
+</g>
+<g id="hat">
+    <g id="XMLID_2_">
+        <path id="XMLID_7_" fill="#282828" d="M272,229l137.8-24.2c0,0,9.8-62.8,0.6-94.9C401.2,77.9,381,41,270,41c0,0-122,0.3-132.5,66
+            c-13.6,85.1,2,107,2,107L272,229z"/>
+        <path id="XMLID_6_" fill="#282828" d="M131.9,201.8c-2.4-0.6-4.7-1.6-7-2.9c-0.4-1.6-0.7-3.3-1.1-4.9
+            c-3.7-15.6-16.1-25.1-32.1-18.1c-23.9,9.9-21.6,36.6-17.8,57.5c3.1,16.9,6.7,33.8,10.5,50.6c14-4.3,28.1-8.5,42.1-12.8
+            c2.9-0.9,8-1.4,9-3.8c1.2-2.9-0.6-9.7-1.7-14.3c7.5-3.8,15-7.7,22.4-11.5c6.9-3.5,6-4,6.6-11.8c1.4-18.8,2.9-37.5,4.3-56.3
+            C161.3,186.5,148.7,206.2,131.9,201.8z"/>
+        <path id="XMLID_5_" fill="#282828" d="M417.3,204.8c2.4-0.6,4.7-1.6,7-2.9c0.4-1.6,0.7-3.3,1.1-4.9c3.7-15.6,16.1-25.1,32.1-18.1
+            c23.9,9.9,21.6,36.6,17.8,57.5c-3.1,16.9-6.7,33.8-10.5,50.6c-14-4.3-28.1-8.5-42.1-12.8c-2.9-0.9-8-1.4-9-3.8
+            c-1.2-2.9,0.6-9.7,1.7-14.3c-7.5-3.8-15-7.7-22.4-11.5c-6.9-3.5-6-4-6.6-11.8c-1.4-18.8-2.9-37.5-4.3-56.3
+            C387.9,189.5,400.5,209.2,417.3,204.8z"/>
+        <path id="XMLID_4_" fill="#282828" d="M84.3,284c0,0,126.6-13.4,183.2-14s197.5,17,197.5,17l-79.4-110.5l-184.5-45L122.5,235
+            L84.3,284z"/>
+        <path id="XMLID_3_" fill="#282828" d="M131.9,201.8c-2.4-0.6-4.7-1.6-7-2.9c-0.4-1.6-0.7-3.3-1.1-4.9
+            c-3.7-15.6-16.1-25.1-32.1-18.1c-23.9,9.9-21.6,36.6-17.8,57.5c3.1,16.9,6.7,33.8,10.5,50.6c14-4.3,28.1-8.5,42.1-12.8
+            c2.9-0.9,8-1.4,9-3.8c1.2-2.9-0.6-9.7-1.7-14.3c7.5-3.8,15-7.7,22.4-11.5c6.9-3.5,6-4,6.6-11.8c1.4-18.8,2.9-37.5,4.3-56.3
+            C161.3,186.5,148.7,206.2,131.9,201.8z"/>
+    </g>
+</g>
+<g id="Sticks">
+    <polygon id="XMLID_1263_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" points="258.3,196.2 309,131 306.8,122.3
+        253.4,193   "/>
+    <g id="XMLID_1229_">
+        <path id="XMLID_1262_" display="none" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M291.7,200.5
+            c-5.5,2.1-11.5,3.3-17.7,3.3c-5.9,0-11.5-1-16.7-2.9l-0.9,1.1c5.5,2,11.4,3.1,17.6,3.1c6.5,0,12.8-1.2,18.5-3.5L291.7,200.5
+            L291.7,200.5z"/>
+        <path id="XMLID_1256_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M227.7,211.7l-2.1-0.7
+            c-0.1,0.7-1.2,4.9-1.5,5.5c-0.3,0.5-0.8,0.9-0.3-0.6c0.4-1.4,0.8-3.4,0.6-5.1l-4.5-1.5c-0.1,1.3-0.9,4.9-1.3,6
+            c-0.4,1-0.6,0.3-0.3-0.6c0.3-0.9,0.5-4.3,0.5-4.6c0-0.3-0.5-0.6-0.5-1.1l-1.2-0.3l-1.8,9l5,0.8c0.7-0.8,1-4,0.9-5.2
+            c-0.1-1.2,0.5-1.2,0.5,0c0,1.2,0.1,4.9-0.3,5.3l5.5,1c0.3-1.3,1-5.6,0.9-6.1C227.7,212.8,227.5,212,227.7,211.7L227.7,211.7z"/>
+        <path id="XMLID_1255_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M331.3,208.5c-0.1,0.6-0.3,0.9-0.5,1.1
+            c-0.2,0.2,0.3,4.4,0.4,5.2c0.1,0.8-0.3,1.4-0.6,0.2c-0.3-1.2-1.2-6.3-1.2-6.3l-4.4,1.3c0.1,0.4,0,0.6-0.1,1
+            c-0.1,0.4,0.1,0.5,0.2,1.2c0.1,0.6,0.5,3.1,0.5,3.8c0,0.7-0.3,1.1-0.6-0.1c-0.3-1.2-1-4.9-1.3-5.4l-2,0.6c0.1,0.4,0,0.7-0.2,1.2
+            c-0.2,0.4,0,0.7,0.1,1.7c0.1,1,0.5,4.8,0.5,4.8l4.6-0.7c-0.1-0.8,0.5-4.6,0.8-5.4c0.3-0.8,0.9-0.8,0.8,0.3
+            c-0.1,1.1,0.2,4.3,0.4,4.9l5-0.7l-1.3-9.2C332,207.8,331.3,207.8,331.3,208.5L331.3,208.5z"/>
+        <path id="XMLID_1254_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M283.4,161.2c0.6-0.8,1.6-1.5,3.1-1.8
+            C286.1,159.3,284.2,159.5,283.4,161.2L283.4,161.2z"/>
+        <path id="XMLID_1253_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M267.2,160.2c0.9,0.2,2,0.7,3,2
+            C270.1,161.7,269.1,160.1,267.2,160.2L267.2,160.2z"/>
+        <path id="XMLID_1252_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M274.2,168c0.9,0.4,1.9,1.1,2.6,2.5
+            C276.7,170,276,168.3,274.2,168L274.2,168z"/>
+        <path id="XMLID_1251_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M281,176.7c0.9,0.4,1.9,1.1,2.6,2.5
+            C283.5,178.8,282.9,177,281,176.7L281,176.7z"/>
+        <path id="XMLID_1248_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M287.7,185.2c0.9,0.4,1.9,1.1,2.6,2.5
+            C290.2,187.3,289.6,185.5,287.7,185.2L287.7,185.2z"/>
+        <path id="XMLID_1247_" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M264.1,179.4c0.9,0.3,2,0.9,2.8,2.3
+            C266.8,181.2,266,179.5,264.1,179.4L264.1,179.4z"/>
+        <polygon id="XMLID_1246_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" points="208,216.4 213.8,217.4 216.2,208.4
+            210.9,206.7 208,216.4       "/>
+        <polygon id="XMLID_1245_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" points="237.2,214.5 229,212.1 227.1,219.5
+            239.2,221.2 258.3,196.2 253.4,193 237.2,214.5       "/>
+        <polygon id="XMLID_1244_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" points="338.1,206.1 332.8,207.7 335.1,216.9
+            341.2,215.9 338.1,206.1         "/>
+        <polygon id="XMLID_1237_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" points="311.8,214 296.2,193.7 291,196.3
+            309.7,220.6 321.8,218.7 320.1,211.5 311.8,214       "/>
+        <path id="XMLID_1236_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" d="M252.7,135.8l-22.9-29l-7,1.7l28.7,36.2
+            C251.3,143.3,252.2,137.5,252.7,135.8L252.7,135.8z"/>
+        <path id="XMLID_1230_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" d="M301.5,129.2c0.9,2.4,2,6.1,2.4,8.4l22.2-28.5
+            l-7.5-2L301.5,129.2L301.5,129.2z"/>
+    </g>
+    <polygon id="XMLID_1228_" fill-rule="evenodd" clip-rule="evenodd" fill="#FBB53B" points="251.5,144.7 296,202.8 302.1,201.6
+        252.7,135.8 229.8,106.8 222.8,108.5     "/>
+</g>
+</svg>
+
+                </div>
+
+                <div class="links">
+                   <h1>Salary Captain Crunch</h1>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
