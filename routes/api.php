fcf41bfa923ab2b6ff60ad4ba4507a44971219b6
ethan <ethan@selftotten.com>
2017-01-18 21:42:53 -0800

    fix the url for client

--- routes/api.php
+++ routes/api.php
@@ -61,7 +61,6 @@ Route::group(['prefix' => 'yahoo', 'middleware'=>'authState'], function () {
     });
 
     Route::get('callback', function (\Illuminate\Http\Request $request) {
-        dd($request);
 
         // Take the code
 
@@ -69,16 +68,16 @@ Route::group(['prefix' => 'yahoo', 'middleware'=>'authState'], function () {
 
 
         $client = new GuzzleHttp\Client();
-        $res = $client->request('POST', 'https://api.login.yahoo.com/oauth2/request_auth?client_id=' . env('CONSUMER_KEY') . '&redirect_uri=https://salarycaptaincrunch.com/api/callback?api_token=' . Auth::user()->api_token . '&response_type=token&language=en-us',
+        $res = $client->request('POST', 'https://api.login.yahoo.com/oauth2/get_token?client_id=' . env('CONSUMER_KEY') . '&redirect_uri=https://salarycaptaincrunch.com/api/callback?api_token=' . Auth::user()->api_token . '&response_type=token&language=en-us',
             [
                 'auth' => [env('CLIENT_KEY'), env('CLIENT_SECRET')],
                 'form_params' => [
                     'grant_type' => 'authorization_code',
-                    'redirect_uri' => 'https://salarycaptaincrunch.com/api/yahoo/token?api_token='.Auth::user()->api_token,
+                    'redirect_uri' => 'https://salarycaptaincrunch.com/api/yahoo/callback/token?api_token='.Auth::user()->api_token,
                     'code' => $request->input('code'),
                 ]
             ]);
 
-        dump($res);
+        dd($res->getBody()->getContents());
     });
 });
