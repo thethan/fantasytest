4518bd45516cae5695e40b6f1bd40540d5cbe61c
ethan <ethan@selftotten.com>
2017-01-23 21:56:38 -0800

    form params

--- app/Yahoo/Fantasy/Players/Get.php
+++ app/Yahoo/Fantasy/Players/Get.php
@@ -7,5 +7,5 @@ use App\Yahoo\YahooService;
 
 class Get extends YahooService
 {
-    use PlayersTrait;
+    protected $uri = '/fantasy/v2/league/359.l.242042/players';
 }
--- app/Yahoo/Fantasy/Players/PlayersTrait.php
+++ app/Yahoo/Fantasy/Players/PlayersTrait.php
@@ -4,6 +4,5 @@ namespace App\Yahoo\Fantasy\Players;
 
 trait PlayersTrait
 {
-    protected $uri = '/fantasy/v2/league/359.l.242042/players';
 
 }
