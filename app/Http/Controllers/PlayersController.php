908de734d494f51c20e5b2fea79129708dfd7449
ethan <ethan@selftotten.com>
2017-01-24 23:40:02 -0800

    added the routes

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Api\Yahoo;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Yahoo\Fantasy\Players\Get;
 use Illuminate\Http\Request;
 
