7550ccec8c285966cb0157b5fffdffb4ff25f7ae
thethan <ethan.totten@demandmedia.com>
2017-02-14 13:21:30 -0800

    add the bugsnag alias to the app service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      
+
     }
 
     /**
@@ -36,6 +36,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+      $this->app->alias('bugsnag.logger', \Illuminate\Contracts\Logging\Log::class);
+      $this->app->alias('bugsnag.logger', \Psr\Log\LoggerInterface::class);
         /**
          * @var AuthManager
          */
