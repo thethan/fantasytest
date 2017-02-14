822f949507f64347e94f967567348074db1842fb
ethan <ethan@selftotten.com>
2017-02-14 01:04:34 -0800

    commit the status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,7 @@
             if (document.readyState === 'complete') {
                 console.log("doc is ready");
                 @if(Auth::check())
-                Echo.channel('App.User.{{Auth::user()->id}}')
+                window.Echo.channel('App.User.{{Auth::user()->id}}')
                     .listen('UserLoggedIntoFantasy', (e) => {
                         console.log(e.user);
                     })
